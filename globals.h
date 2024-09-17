43aab0de27b2eb55a5e993cae450374db0e8b1a5
joshy <janez.rotman@tum.de>
2024-09-17 18:27:55 +0200

    separate typedefs and registers to seperate files so they can be used without including the superSTL

--- globals.h
+++ globals.h
@@ -16,23 +16,7 @@ extern "C" {
 #include <sys/ptrace.h>
 }
 
-typedef __SIZE_TYPE__ size_t;
-typedef unsigned long long W64;
-typedef signed long long W64s;
-typedef unsigned int W32;
-typedef signed int W32s;
-typedef unsigned short W16;
-typedef signed short W16s;
-typedef unsigned char byte;
-typedef unsigned char W8;
-typedef signed char W8s;
-#define null NULL
-
-#ifdef __x86_64__
-typedef W64 Waddr;
-#else
-typedef W32 Waddr;
-#endif
+#include <typedefs.h>
 
 #ifdef __cplusplus
 
